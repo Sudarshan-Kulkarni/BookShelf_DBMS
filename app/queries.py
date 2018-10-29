1bacd8b8c1a106ac44a419353dd4833530922f45
SupernovaHysteria <sudarshankul500@gmail.com>
2018-10-29 18:04:36 +0530

    Lending section table

--- app/queries.py
+++ app/queries.py
@@ -12,26 +12,27 @@ def connect():
 def create_db():
     conn,cur = connect()
     
-    #cur.execute("DROP TABLE user_info;")
-    #cur.execute("DROP TABLE lending_section;") 
-    #cur.execute("DROP TABLE reading_section;") 
-    #cur.execute("DROP TABLE books")
-    #cur.execute("DROP TABLE incomplete_transaction;")
+    # cur.execute("DROP TABLE user_info;")
+    # cur.execute("DROP TABLE lending_section;") 
+    # cur.execute("DROP TABLE reading_section;") 
+    # cur.execute("DROP TABLE books")
+    # cur.execute("DROP TABLE incomplete_transaction;")
     
 
     cur.execute("CREATE TABLE user_info(UID integer primary key autoincrement,name varchar(50) ,street varchar(30) ,city varchar(20) ,state varchar(20) ,country varchar(20) ,contact_no int(10) ,email varchar(50),password varchar(20));")
     cur.execute("INSERT INTO user_info values(101,'Naruto','Ashokapuram','Mysuru','Karnataka','India',9016387328,'naruto@gmail.com','nar123');")
     cur.execute("INSERT INTO user_info values(null,'Sasuke','Indiranagar','Bangalore','Karnataka','India',8867352489,'sasuke@gmail.com','sas123');")
 
-    cur.execute("CREATE TABLE lending_section(LID int ,ISBN int(10) ,av int(1) ,RID int ,foreign key(ISBN) references books(ISBN));")
+    cur.execute("CREATE TABLE lending_section(LID integer ,ISBN int(10) ,av int(1) ,RID integer ,foreign key(ISBN) references books(ISBN));")
     cur.execute("INSERT INTO lending_section values(101 ,1000000000 ,1 , null);")
+
     """
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     Gotta take care of the book_namees and authors so as to perform joins!!!!!!!!!!!!!
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     """
-    cur.execute("CREATE TABLE reading_section(RID int ,ISBN int(10) , due_date date, extn_count int default 0 ,LID int ,foreign key(ISBN) references books(ISBN));")
+    cur.execute("CREATE TABLE reading_section(RID integer ,ISBN int(10) , due_date date, extn_count int default 0 ,LID integer ,foreign key(ISBN) references books(ISBN));")
 
     cur.execute("CREATE TABLE books(ISBN int(10) ,book_name varchar(40) ,author varchar(50) ,popularity int DEFAULT 0);")
     cur.execute("INSERT INTO books values(1000000000,'Data Communications and Networking' ,'Forouzan',null);")
@@ -39,7 +40,7 @@ def create_db():
     cur.execute("INSERT INTO books values(1000000002,'The Database Book' ,'Narain Gehani',null);")
     cur.execute("INSERT INTO books values(1000000003,'Modern Operating Systems' ,'Andrew S. Tanenbaum',null);")
 
-    cur.execute("CREATE TABLE incomplete_transaction(RID int ,ISBN int(10) ,LID int ,foreign key(ISBN) references books(ISBN));")
+    cur.execute("CREATE TABLE incomplete_transaction(RID integer ,ISBN int(10) ,LID integer ,foreign key(ISBN) references books(ISBN));")
 
 
     # create a trigger later for inserting a transaction into incomplete transaction table.
@@ -101,18 +102,22 @@ def update_password(User_Data,new_password):
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_lent_books(User_Data):
     conn,cur = connect()
-    cur.execute("SELECT ISBN,book_name,author,av,name,email,contact_no FROM lending_section NATURAL JOIN books JOIN user_info ON lending_section.RID = user_info.UID where LID = ?;",(User_Data[0],))
+    #cur.execute("SELECT L.ISBN,B.book_name,B.author,L.av,U.name,U.email,U.contact_no FROM lending_section L,books B,user_info U where L.ISBN=B.ISBN AND L.RID=U.UID AND L.LID = ?;",(User_Data[0],))
+    #cur.execute("SELECT ISBN,book_name,author,av from lending_section NATURAL JOIN books INNER JOIN user_info ON lending_section.RID=user_info.UID where LID=?;",(User_Data[0],))
+    cur.execute("SELECT L.ISBN,B.book_name,B.author,L.av,U.name,U.email,U.contact_no FROM lending_section L JOIN books B ON L.ISBN=B.ISBN LEFT OUTER JOIN user_info U ON L.RID=U.UID where L.LID= ?;",(User_Data[0],))
     lent_books = cur.fetchall()
-    lent_books = list(lent_books)
+    for i in range(len(lent_books)):
+        lent_books[i] = list(lent_books[i])
     print(lent_books)
-    for book in lent_books:
-        for i in book:
-            if i==3:
-                if book[i]==0:
-                    book[i] = 'Lent'
-                    book[i+1] = '--'
-                    book[i+2] = '--'
-                    break
-                else:
-                    book[i] = 'Not Lent' 
+    for i in range(len(lent_books)):
+        for j in range(len(lent_books[i])):
+            if j==3:
+                 if lent_books[i][j]==0:
+                     lent_books[i][j] = 'Lent'
+                     break
+                 else:
+                     lent_books[i][j] = 'Not Lent' 
+                     lent_books[i][j+1] = '--'
+                     lent_books[i][j+2] = '--'
+                     lent_books[i][j+3] = '--'
     return lent_books
